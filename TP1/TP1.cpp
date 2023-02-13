47cb2246bb85ef987a95643672925eba7f3b37b9
Adele <56159423+Adeleimpa@users.noreply.github.com>
2023-02-13 12:34:36 +0100

    folder textures

--- TP1/TP1.cpp
+++ TP1/TP1.cpp
@@ -313,7 +313,7 @@ int main( void )
 
     // on charge et on genere la texture a l'aide de l'image texture (.png)
     int width, height, nrChannels;
-    unsigned char *data = stbi_load("carre.jpg", &width , &height , &nrChannels , 0);
+    unsigned char *data = stbi_load("textures/carre.jpg", &width , &height , &nrChannels , 0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE , data);
         glGenerateMipmap(GL_TEXTURE_2D); }
